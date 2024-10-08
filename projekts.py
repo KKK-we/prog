3483a20af64b61ade737669c058925b2997ca3bd
KKK-we <lapinskristers@gmail.com>
2024-10-08 13:35:34 +0300

    Update projekts.py

--- projekts.py
+++ projekts.py
@@ -1,27 +1,48 @@
+
+def dalisana(num1, num2):
+    if num2 == 0:
+        return "Nevar dalīt ar 0!!!"
+    return num1/num2
+
+def reizinasana(num1,num2):
+    return num1*num2
+
+def saskaitisana(num1,num2):
+    return num1+num2 
+    
+def atnemsana(num1,num2):
+    return num1-num2
+
 def calculator():
     print("Welcome to the Calculator!")
 
-    a = int(input("Enter the first number: "))
-    b = int(input("Enter the second number: "))
-    
-    operation = input("Enter operation (+, -, *, /): ")
+    a = input("Enter the first number: ")
+    b = input("Enter the second number: ")
+    if a.isnumeric() and b.isnumeric():
+        a = int(a)
+        b = int(b)
+        operation = input("Enter operation (+, -, *, /): ")
 
-    if operation == "+":
-        result = a + b
-    elif operation == "-":
-        result = a - b
-    elif operation == "*":
-        result = a * b
-    elif operation == "/":
-        if b == 0:
-            print("Error: Cannot divide by zero!")
+        if operation == "+":
+            result = saskaitisana(a,b)
+        elif operation == "-":
+            result = atnemsana(a,b)
+        elif operation == "*":
+            result = reizinasana(a,b)
+        elif operation == "/":
+            result = dalisana(a,b)
+        else:
+            print("Error")
             return
-        result = a / b
+
+        print("The result is:", result)
     else:
-        print("Invalid operation!")
-        return
-
-    print("The result is:", result)
+        print("Ievadi skaitļus")
 
 
-calculator()
+answer = "yes"
+
+
+while answer == "yes":
+    calculator()
+    answer = input("Vai vēlies turpināt? (ievadi 'yes'): ")
